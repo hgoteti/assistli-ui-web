bf162e4d373db8f8c6118b264045317915a4cbfd
Hemanth Goteti <hemanth.goteti@gmail.com>
2025-02-20 19:30:17 +0530

    Add theme support with Radix UI components and Tailwind configuration

---
+++ src/app/layout.jsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from "@/components/theme-provider.jsx"
+
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={true}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+} 
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} 
